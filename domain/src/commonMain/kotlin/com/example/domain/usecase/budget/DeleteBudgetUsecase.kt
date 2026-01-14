b27dfa52744642cae1c887a9f587f99bcccdb4e0
Mohammed Khubaib C <mohammedkhubaib10@gmail.com>
2026-01-14 18:44:07 +0530

    Budget deletion will also delete expenses

--- domain/src/commonMain/kotlin/com/example/domain/usecase/budget/DeleteBudgetUsecase.kt
+++ domain/src/commonMain/kotlin/com/example/domain/usecase/budget/DeleteBudgetUsecase.kt
@@ -1,9 +1,20 @@
 package com.example.domain.usecase.budget
 
 import com.example.domain.repository.BudgetRepository
+import com.example.domain.repository.ExpenseRepository
 
-class DeleteBudgetUsecase(private val budgetRepository: BudgetRepository) {
+class DeleteBudgetUsecase(
+    private val budgetRepository: BudgetRepository,
+    private val expenseRepository: ExpenseRepository
+) {
     suspend operator fun invoke(userId: String, budgetId: String) {
+        val expenses =
+            expenseRepository.getExpensesByBudget(userId, budgetId)
+
+        // 2️⃣ Delete all expenses
+        expenses.forEach {
+            expenseRepository.deleteExpense(it.expenseId)
+        }
         budgetRepository.deleteBudget(userId = userId, budgetId = budgetId)
     }
 }
